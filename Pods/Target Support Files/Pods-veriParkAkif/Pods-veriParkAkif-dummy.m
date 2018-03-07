07dd30742bf7df3c8ef25524a3744ca26c5d6b8b
akifdemirezen <ko@ko-macbook-pro.local>
2018-03-07 17:02:17 +0300

    ilk commit

---
+++ Pods/Target Support Files/Pods-veriParkAkif/Pods-veriParkAkif-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_veriParkAkif : NSObject
+@end
+@implementation PodsDummy_Pods_veriParkAkif
+@end
---
+++ veriParkAkif/CustomTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTabBarController.swift
+//  veriParkAkif
+//
+//  Created by Akif_PC on 7.03.2018.
+//  Copyright © 2018 akif demirezen. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let imageView = UIImageView(image: UIImage(named: "veriparklogo"))
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: (self.navigationController?.navigationBar.frame.width)!, height: (self.navigationController?.navigationBar.frame.height)!))
+        imageView.frame = titleView.bounds
+        titleView.addSubview(imageView)
+        self.navigationItem.titleView = titleView
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
